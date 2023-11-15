31ce588b8a02eb6af5a653feab7a6487296519d8
Rockwell70 <rochwellshabani@gmail.com>
2023-11-15 11:03:56 -0800

    c boook work

--- .vscode/hello.cpp
+++ .vscode/hello.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello People\nWho do you think you are?" << endl;
+    int start, end;
+    char ch = '\t';
+    string mystring = "abcdef";
+
+    start = 1;
+    end = 11;
+    end += 45;
+    cout << "Hello People Who do you think" << ch << ch << ch << "you are?\n" << endl;
+    cout << end - start << endl;
+    cout << mystring[2] << endl;
     return 0;
 }
