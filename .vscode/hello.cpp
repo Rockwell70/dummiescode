0230be698070a921c11ff331428b39f5ac79f207
Rockwell70 <rockwellshabani@gmail.com>
2023-11-16 05:47:49 -0800

    s

--- .vscode/hello.cpp
+++ .vscode/hello.cpp
@@ -6,12 +6,17 @@ int main()
 {
     int start, end;
     char ch = '\t';
+    string result;
     string mystring = "abcdef";
 
     start = 1;
     end = 11;
     end += 45;
-    cout << "Hello People Who do you think" << ch << ch << ch << "you are?\n" << endl;
+    result = (start == end) ? "equal" : "not equal";
+    cout << "Hello People Who do you think" << ch << "you are?\n" << endl;
+    cout << "False is actually a number " << result << endl;
+    cout << "Start and End are equal? " << result << endl;
+
     cout << end - start << endl;
     cout << mystring[2] << endl;
     return 0;
