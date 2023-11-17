e9d6f28f6b1b6cc8b527e4ca6dea68e304c2540c
Rockwell70 <rochwellshabani@gmail.com>
2023-11-17 15:30:29 -0800

    testing work

---
+++ testing/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    int mynum, testnum, testresult;
+    mynum = 10;
+    testnum = 4;
+    testresult = mynum > testnum;
+    std::cout << "Mynum is greater then the test? " << testresult  << std::endl;
+
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
