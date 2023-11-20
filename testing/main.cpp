4eab0c123427ece9971a821695f51ce877d966b1
Rockwell70 <rochwellshabani@gmail.com>
2023-11-20 15:57:34 -0800

    added gitignore

--- testing/main.cpp
+++ testing/main.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 
 using namespace std;
-int main() {
+int mainvi() {
 
-    int i;
-    cout << "Enter an number." << endl;
-    cin >> i;
+//    int i;
+//    cout << "Enter an number." << endl;
+//    cin >> i;
+//
+//    if (i > 100){
+//        cout << "your number is greater than 100." << endl;
+//    }
+//
+//    else if (i > 10)
+//    {
+//        cout << "your number is greater than 10." << endl;
+//    }
+//
+//    else
+//    {
+//        cout << "What's the matter with you!" << endl;
+//    }
 
-    if (i > 100){
-        cout << "your number is greater than 100." << endl;
-    }
+//Looping
+    int x = 0;
 
-    else if (i > 10)
+    for (int i = 1; i <= 100; i++)
     {
-        cout << "your number is greater than 10." << endl;
+        x += i;
+        cout << x << endl;
     }
 
-    else
-    {
-        cout << "What's the matter with you!" << endl;
-    }
 
 
     return 0;
