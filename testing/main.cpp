a4899980e277fe1a55ab604ac67243133c5ae9be
Rockwell70 <rochwellshabani@gmail.com>
2023-11-22 15:20:11 -0800

    updated looping

--- testing/main.cpp
+++ testing/main.cpp
@@ -22,17 +22,27 @@ int main() {
 //    }
 
 //Looping
-    int x = 0;
+//    int x = 0;
+//
+//    for (int i = 1; i <= 100; i++) {
+//        x += i;
+//
+//        if (x % 2 == 0) {
+//            cout << "i is " << i << " and " << x << " is even." << endl;
+//        } else {
+//            cout << "i is " << i << " and " << x << " is odd." << endl;
+//        }
+//
+//    }
 
-    for (int i = 1; i <= 100; i++) {
-        x += i;
+//Looping with multiple variables
 
-        if (x % 2 == 0) {
-            cout << x << " is even." << endl;
-        } else {
-            cout << x << " is odd." << endl;
-        }
+    string A = "Hello";
+    string B = "1122334455";
 
+    for (autot i = 0, j = 0; i < 5; i++, j += 2)
+    {
+        cout << A[i] << B[j] << endl;
     }
 
     return 0;
