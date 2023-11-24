0bb1c400aeb6c37ab589e0cabfd8308532bf28a0
Rockwell70 <rockwellshabani@gmail.com>
2023-11-24 10:52:56 -0800

    added mac version of C++ code

--- mac_code/main.cpp
+++ mac_code/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+int func1() {
+    std::cout << "This is function 1" << std::endl;
     return 0;
 }
+
+int func2() {
+    std::cout << "This is function 2\n" << std::endl;
+    return 0;
+}
+
+int main() {
+    int i;
+    cout << "Enter either 1 or 2\n" << endl;
+    cin >> i;
+    if (i == 1){
+        func1();
+
+    } else if (i == 2) {
+        func2();
+    }
+
+    else{
+        cout << "error!" << endl;
+    }
+
+
+}
+
+
