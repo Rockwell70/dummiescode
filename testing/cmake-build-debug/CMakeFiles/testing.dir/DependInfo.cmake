dec0ba5377b41983185553636b01d1a2be0d8077
Rockwell70 <rockwellshabani@gmail.com>
2023-11-28 14:02:39 -0800

    added OpenXLSX to lib

--- testing/cmake-build-debug/CMakeFiles/testing.dir/DependInfo.cmake
+++ testing/cmake-build-debug/CMakeFiles/testing.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/rockwell/projects/dummiescode/testing/main.cpp" "CMakeFiles/testing.dir/main.cpp.o" "gcc" "CMakeFiles/testing.dir/main.cpp.o.d"
+  "C:/Users/shabje/CLionProjects/dummiescode/testing/main.cpp" "CMakeFiles/testing.dir/main.cpp.obj" "gcc" "CMakeFiles/testing.dir/main.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- testing/cmake-build-debug/CMakeFiles/testing.dir/cmake_clean.cmake
+++ testing/cmake-build-debug/CMakeFiles/testing.dir/cmake_clean.cmake
@@ -1,8 +1,10 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/testing.dir/main.cpp.o"
-  "CMakeFiles/testing.dir/main.cpp.o.d"
-  "testing"
-  "testing.pdb"
+  "CMakeFiles/testing.dir/main.cpp.obj"
+  "CMakeFiles/testing.dir/main.cpp.obj.d"
+  "output/libtesting.dll.a"
+  "output/testing.exe"
+  "output/testing.exe.manifest"
+  "output/testing.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
